6e76e61e1ebe6c87ff194bfc5505042861ad5a29
bar <bsahar1990@gmail.com>
2020-07-15 19:21:38 +0300

    a bit of refactoring

--- popup/popup.js
+++ popup/popup.js
@@ -4,6 +4,7 @@ function updateTimer(page) {
   let currTime = page.currTime;
   let currInterval = page.currInterval;
   let isFocused = page.isFocused;
+  var maxInterval = 4;
 
   var minutes = Math.floor((currTime % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((currTime % (1000 * 60)) / 1000);
@@ -13,8 +14,10 @@ function updateTimer(page) {
   if (seconds < 10) {
     seconds = '0' + seconds;
   }
-  debugger;
+  
   $('#timer').text(minutes + ':' + seconds);
+  $('#iteration').text(currInterval + '/' + maxInterval);
+
 }
 
 function onError(error) {
@@ -27,11 +30,15 @@ function timerTick() {
 
 function initClickListener() {
   $('#start').on("click", (e) => {
+    $('#start').hide();
+    $('#pause').show();
     browser.runtime.sendMessage({
       command:"start"
     })
   });
   $('#pause').on("click", (e) => {
+    $('#start').show();
+    $('#pause').hide();
     browser.runtime.sendMessage({
       command:"pause"
     })
@@ -43,6 +50,7 @@ function initClickListener() {
   });
 }
 
+$('#pause').hide();
 initClickListener();
 timerTick();
 setInterval(timerTick, 500);
